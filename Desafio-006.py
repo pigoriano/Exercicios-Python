812810591fd755d5992934d085506e1dc2bad453
Igor Oliveira <igors13@gmail.com>
2024-07-15 16:45:48 -0300

    Formatei o código melhor

--- Desafio-006.py
+++ Desafio-006.py
@@ -5,6 +5,9 @@ n = int(input('Digite seu número: '))
 
 dobro = n * 2
 triplo = n * 3
-raiz = n ** 0.5
+raiz = n ** (1/2)
 
-print ('\nAnalizando o valor: {},\nSeu dobro é: {}\nSeu triplo é: {}\nSua Raiz Quadrada é: {}'.format(n,dobro,triplo,raiz))
+print ('\nAnalizando o valor: {},\nSeu dobro é: {}\nSeu triplo é: {}\nSua Raiz Quadrada é: {:.2f}'.format(n,dobro,triplo,raiz))
+
+#Quebra de linha \n
+#Formatar para aparecer 2 digitos após o . {:.2f}
