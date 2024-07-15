5c9fa0943469f79b5f0f2e4eb3d16dd7bf9bbe7d
Igor Oliveira <igors13@gmail.com>
2024-07-15 16:26:50 -0300

    Resolvi os desafios 005 e 006

--- Desafio-005.py
+++ Desafio-005.py
@@ -1,2 +1,9 @@
 print ('========== DESAFIO 005 ==========') 
 #Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n = int(input("Digite um número: "))
+
+antecessor = n - 1
+sucessor = n + 1
+
+print ('Analizando o valor: {}; seu antecessor é {}; e o sucessor é {}'.format(n,antecessor,sucessor))
--- Desafio-006.py
+++ Desafio-006.py
@@ -1,2 +1,10 @@
 print ('========== DESAFIO 006 ==========') 
 #Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
+
+n = int(input('Digite seu número: '))
+
+dobro = n * 2
+triplo = n * 3
+raiz = n ** 0.5
+
+print ('\nAnalizando o valor: {},\nSeu dobro é: {}\nSeu triplo é: {}\nSua Raiz Quadrada é: {}'.format(n,dobro,triplo,raiz))
