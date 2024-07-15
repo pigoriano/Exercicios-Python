6cb733376d2ecfe473fd0fee7aa50e418f932291
Igor Oliveira <igors13@gmail.com>
2024-07-15 15:59:10 -0300

    Criei do desafio 005 até o desafio 013

---
+++ Desafio-007.py
@@ -0,0 +1,2 @@
+print ('========== DESAFIO 007 ==========') 
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostrea a sua média.
---
+++ Desafio-008.py
@@ -0,0 +1,6 @@
+print ('========== DESAFIO 008 ==========') 
+#Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milímetros.
+
+metro = int(input('Digite a quantidade de metros'))
+
+print( 'O valor em centímetros é {}'.format())
