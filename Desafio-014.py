849011267d89cc7a20c3ede9adf352cea9817433
Igor Oliveira <igors13@gmail.com>
2024-07-16 17:12:07 -0300

    criei e resolvi desafio 014

---
+++ Desafio-014.py
@@ -0,0 +1,8 @@
+print ('========== DESAFIO 013 ==========') 
+#Crie um conversor de temperatura de Celsius para Fahrenheit
+
+celsius = float(input('Informe a temperatura em °C: '))
+
+f = (celsius * 9/5) + 32
+
+print ('A temperatura de {}°C corresponde a {}°F!'.format(celsius,f))
