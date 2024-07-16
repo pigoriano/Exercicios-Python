84f07cd4ba1905029d0bdca396c34f3a828b345f
Igor Oliveira <igors13@gmail.com>
2024-07-16 15:53:06 -0300

    resolvi desafio 8 e 9

--- Desafio-008.py
+++ Desafio-008.py
@@ -3,8 +3,16 @@ print ('========== DESAFIO 008 ==========')
 
 metro = int(input('Digite a quantidade de metros: '))
 
+km = metro / 1000
+hm = metro / 100
+dam = metro / 10
+dm = metro * 10
 cm = metro * 100
 mm = metro * 1000
 
-print( 'O valor em centímetros é: {}'.format(cm))
-print(' O valor em milímetros é: {}'.format(mm))
+print('O valor em km é: {}km'.format(km))
+print('O valor em hectometros é: {}hm'.format(hm))
+print('O valor em decametro é: {}dam'.format(dam))
+print('O valor em decímetro é: {}dm'.format(dm))
+print('O valor em centímetros é: {}cm'.format(cm))
+print('O valor em milímetros é: {}mm'.format(mm))
