e8f5dadb80d45d19f0dedc5189f0f65bd30d60ec
Igor Oliveira <igors13@gmail.com>
2024-07-18 11:07:05 -0300

    Adicionado 6 desafios
    22, 23, 24, 25, 26, 27

---
+++ Desafio-022.py
@@ -0,0 +1,6 @@
+print ('========== DESAFIO 022 ==========') 
+#Crie um programa que leia o nome completo de uma pessoa e mostre:
+#O nome com todas as letras maiúsculas
+#O nome com todas minúsculas.
+#Quantas letras ao todo (sem considerar espaços)
+#Quantas letra tem o primeiro nome.
---
+++ Desafio-024.py
@@ -0,0 +1,2 @@
+print ('========== DESAFIO 024 ==========') 
+#Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santo"
