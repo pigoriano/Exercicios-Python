a9d01c69574480b8752ad88be0e2cefc4619df19
Igor Oliveira <igors13@gmail.com>
2024-07-22 14:05:51 -0300

    Adicionado novos desafios
    sobre if else

---
+++ Desafio-029.py
@@ -0,0 +1,4 @@
+print ('========== DESAFIO 029 ==========') 
+#Escreva um programa que leia a velocidade de um carro.
+#Se ele ultrapassar 80km/h, mostre uma msg dizendo que ele foi multado
+#A multa vai custar R$7,00 por cada km acima do limite.
---
+++ Desafio-030.py
@@ -0,0 +1,2 @@
+print ('========== DESAFIO 030 ==========') 
+#Crie um programa que leia um numero inteiro e mostre na tela se ele é par ou impar
