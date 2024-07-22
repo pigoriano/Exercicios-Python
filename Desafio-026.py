6ce4103c3a534c3a252fd26fec70b7c304d1b115
Igor Oliveira <igors13@gmail.com>
2024-07-22 12:43:40 -0300

    exercicio 026 resolvido

--- Desafio-026.py
+++ Desafio-026.py
@@ -3,3 +3,8 @@ print ('========== DESAFIO 026 ==========')
 #Quantas vezes aparece a letra "A".
 #Em que posição ela aparece a primeira vez.
 #Em que posição ela aparece a última vez.
+
+frase = str(input('digite uma frase: ')).upper().strip()
+print ('A letra A aparece {} vezes na frase'.format(frase.count('A')))
+print ('A letra A apareceu na posição {}'. format(frase.find('A')+1))
+print ('A ultima letra A apareceu na posição {}'.format(frase.rfind('A')+1))
