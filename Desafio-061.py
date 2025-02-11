4e350c50c14151c871858944fdaf24849dc93ca6
Igor Oliveira <igors13@gmail.com>
2025-02-10 22:10:52 -0300

    Update Desafio-061.py

--- Desafio-061.py
+++ Desafio-061.py
@@ -1,2 +1,14 @@
 print ('========== DESAFIO 061 ==========') 
-# Refaça o desafio 051, lendro o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressao usando a estrutura while
+# Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressao usando a estrutura while
+
+print ('Gerador de PA')
+print ('-=' * 10)
+primeiro = int (input ('Primeiro termo: '))
+razão = int(input ('Razão da PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print ('{} > '.format(termo), end='')
+    termo += razão
+    cont += 1
+print ('fim')
